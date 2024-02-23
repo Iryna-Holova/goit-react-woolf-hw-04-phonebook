14e2670b78319aae615d27a4d6bb50737cd0853a
Iryna Holova <gis.golova@gmail.com>
2024-02-23 23:32:06 +0200

    add start files

---
+++ src/components/App.jsx
@@ -0,0 +1,91 @@
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+import Section from './Section/Section';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts) this.setState({ contacts: JSON.parse(savedContacts) });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  handleAddContact = data => {
+    const normalizedName = data.name.toLowerCase();
+    const isExist = this.state.contacts.some(
+      contact => contact.name.toLowerCase() === normalizedName
+    );
+
+    if (isExist) {
+      throw new Error(`${data.name} is already in contacts.`);
+    }
+
+    this.setState(prevState => ({
+      contacts: [{ ...data, id: nanoid() }, ...prevState.contacts],
+    }));
+  };
+
+  handleRemoveContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(({ id }) => id !== contactId),
+    }));
+  };
+
+  handleFilterChange = evt => {
+    this.setState({ filter: evt.target.value });
+  };
+
+  handleFilterClear = () => {
+    this.setState({ filter: '' });
+  };
+
+  filterContacts() {
+    const normalizedFilter = this.state.filter.toLowerCase();
+    return this.state.contacts.filter(({ name }) =>
+      name.toLowerCase().includes(normalizedFilter)
+    );
+  }
+
+  render() {
+    const contacts = this.filterContacts();
+    const { filter } = this.state;
+    return (
+      <>
+        <Section>
+          <h1>Phonebook</h1>
+        </Section>
+        <Section>
+          <ContactForm onSubmit={this.handleAddContact} />
+        </Section>
+        <Section>
+          <Filter
+            value={filter}
+            onFilterChange={this.handleFilterChange}
+            onFilterClear={this.handleFilterClear}
+          />
+        </Section>
+        <Section title={filter ? `Results: ${contacts.length}` : 'Contacts'}>
+          <ContactList
+            contacts={contacts}
+            onRemove={this.handleRemoveContact}
+            filter={filter}
+          />
+        </Section>
+      </>
+    );
+  }
+}
+
+export default App;
